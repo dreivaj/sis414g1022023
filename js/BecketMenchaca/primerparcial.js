8ec4c4737b9370a80bb475b01a3adacfe473c0d4
Becket <becketvelasquez123@gmail.com>
2023-10-19 23:08:10 -0400

    PrimerParcailEntregaFinal

--- js/BecketMenchaca/primerparcial.js
+++ js/BecketMenchaca/primerparcial.js
@@ -1,9 +1,18 @@
-const menu = document.querySelector('.menu');
-const men = document.querySelector('.menu-navegacion');
+menú
+const menuLinks = document.querySelectorAll('.menu a');
 
-console.log(menu)
-console.log(men)
+// Agrega un evento de clic a cada enlace
+menuLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Evita el comportamiento predeterminado de anclaje
+        const targetId = link.getAttribute('href').substring(1); // Obtiene el ID del objetivo
+        const targetElement = document.getElementById(targetId);
 
-menu.addEventListener('click', ()=>{
-    men.classList.toggle("spread")
-})
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth' // Desplazamiento suave
+            });
+        }
+    });
+});
