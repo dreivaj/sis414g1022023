77a7a66862370596dd58cfdadd716e6967cf9e3b
HP laptop <torrezk397@gmail.com>
2023-11-03 01:04:07 -0400

    arreglando repositorio

---
+++ js/KevinTorrezVedia/lab1.js
@@ -0,0 +1,24 @@
+// Declarar e inicializar las variables para los contactos
+var nombre1 = "Wright Maxwell";
+var telefono1 = "(0191) 719 6495";
+var correo1 = "Curabitur.egestas.nunc@nonummyac.es";
+
+var nombre2 = "Raja Villarreal";
+var telefono2 = "0866 398 2895";
+var correo2 = "posuere.vulputate@sed.com";
+
+var nombre3 = "Helen Richards";
+var telefono3 = "0800 1111";
+var correo3 = "libero@convallis.edu";
+
+// Mostrar información sobre el primer contacto
+console.log("Primer contacto:");
+console.log("Nombre:", nombre1);
+console.log("Teléfono:", telefono1);
+console.log("Correo electrónico:", correo1);
+
+// Mostrar información sobre el último contacto
+console.log("\nÚltimo contacto:");
+console.log("Nombre:", nombre3);
+console.log("Teléfono:", telefono3);
+console.log("Correo electrónico:", correo3);
